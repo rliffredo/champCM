0ede2fe564ed5b6761a21480be42f88946007f90
Roberto Liffredo <rliffredo@outlook.com>
2015-05-27 12:30:13 +0200

    Improved error message in case of issues when using the local bootstrap file. Reformatted the file to 4-spaces tabulations.

--- Bootstrap.cs
+++ Bootstrap.cs
@@ -1,56 +1,66 @@
-﻿using Ionic.Zip;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using Ionic.Zip;
 
 namespace champ
 {
-  public class Bootstrap
-  {
-    private DirectoryInfo _source;
-    private string _uncLocation;
-
-    public Bootstrap(string sourceDirectory, string bootstrapUNCLocation)
+    public class Bootstrap
     {
-      _source = new DirectoryInfo(sourceDirectory);
-      _uncLocation = bootstrapUNCLocation;
-    }
+        private DirectoryInfo _source;
+        private string _uncLocation;
 
-    public void Run()
-    {
-      // Download the template
-      var targetFile = Path.GetTempFileName();
-      if (String.IsNullOrEmpty(_uncLocation))
-      {
-        var client = new WebClient();
-          const string downloadUrl = "https://github.com/lukevenediger/champ-bootstrap/releases/download/v1.0/champ-bootstrap.zip";
-        Console.WriteLine("Downloading champ-bootstrap.zip (github.com)");
-        try
+        public Bootstrap(string sourceDirectory, string bootstrapUNCLocation)
         {
-          client.DownloadFile(downloadUrl, targetFile);
+            _source = new DirectoryInfo(sourceDirectory);
+            _uncLocation = bootstrapUNCLocation;
         }
-        catch (System.Net.WebException e)
+
+        public void Run()
         {
-          Console.WriteLine("Cannot download bootstrap. Reason: {0}.\nYou might want to manually download it from ({1}) and then use -s option", e.Message, downloadUrl);
-          return;
+            // Download the template
+            var targetFile = Path.GetTempFileName();
+            if (String.IsNullOrEmpty(_uncLocation))
+            {
+                var client = new WebClient();
+                const string downloadUrl =
+                    "https://github.com/lukevenediger/champ-bootstrap/releases/download/v1.0/champ-bootstrap.zip";
+                Console.WriteLine("Downloading champ-bootstrap.zip (github.com)");
+                try
+                {
+                    client.DownloadFile(downloadUrl, targetFile);
+                }
+                catch (WebException e)
+                {
+                    Console.WriteLine(
+                        "Cannot download bootstrap. You might want to manually download it from ({1}) and then use -s option.\n" +
+                        "Exception details: {0}",
+                        e.Message, downloadUrl);
+                    return;
+                }
+            }
+            else
+            {
+                try
+                {
+                    var file = new FileInfo(_uncLocation);
+                    Console.WriteLine("Copying " + file.Name + " from " + file.Directory.FullName);
+                    file.CopyTo(targetFile, true);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(
+                        "Cannot copy the bootstrap. Please make sure the file is spelled correctly, and is accessible.\n" +
+                        "Exception details: {0}", e.Message);
+                    return;
+                }
+            }
+            // Unzip it to the source directory
+            using (var zipFile = ZipFile.Read(targetFile))
+            {
+                zipFile.ExtractAll(_source.FullName, ExtractExistingFileAction.OverwriteSilently);
+            }
+            Console.WriteLine("Done.");
         }
-      }
-      else
-      {
-        var file = new FileInfo(_uncLocation);
-        Console.WriteLine("Copying " + file.Name + " from " + file.Directory.FullName);
-        file.CopyTo(targetFile, true);
-      }
-      // Unzip it to the source directory
-      using (var zipFile = ZipFile.Read(targetFile))
-      {
-        zipFile.ExtractAll(_source.FullName, ExtractExistingFileAction.OverwriteSilently);
-      }
-      Console.WriteLine("Done.");
     }
-  }
 }
